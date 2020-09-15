72b14f99c003b351ccdd4ad90adbaeb44f7957fe
RafaelMenezess <rafa.rcm@hotmail.com>
2020-09-14 22:01:20 -0300

    AutoMapper

--- RallyDakar.API/Controllers/PilotoController.cs
+++ RallyDakar.API/Controllers/PilotoController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.JsonPatch;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using RallyDakar.API.Modelo;
 using RallyDakar.Dominio.Entidades;
 using RallyDakar.Dominio.Interfaces;
 using System;
@@ -13,9 +15,10 @@ namespace RallyDakar.API.Controllers
     [Route("api/pilotos")]
     public class PilotoController : ControllerBase
     {
-        IPilotoRepositorio _pilotoRepositorio;
+        private readonly IPilotoRepositorio _pilotoRepositorio;
+        private readonly IMapper _mapper;
 
-        public PilotoController(IPilotoRepositorio pilotoRepositorio)
+        public PilotoController(IPilotoRepositorio pilotoRepositorio, IMapper _mapper)
         {
             _pilotoRepositorio = pilotoRepositorio;
         }
@@ -26,42 +29,50 @@ namespace RallyDakar.API.Controllers
             try
             {
                 var piloto = _pilotoRepositorio.Obter(id);
+
                 if (piloto == null)
                     return NotFound();
 
-                return Ok(piloto);
+                var pilotoModelo = _mapper.Map<PilotoModelo>(piloto);
+
+                return Ok(pilotoModelo);
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "ocorreu um erro interno no sistema. Por favor entre em contato com o suporte");
+                return StatusCode(500, "Ocorreu um erro interno no sistema. Por favor entre em contato com o suporte");
             }
         }
 
         [HttpPost]
-        public IActionResult AdicionarPiloto([FromBody] Piloto piloto)
+        public IActionResult AdicionarPiloto([FromBody] PilotoModelo pilotoModelo)
         {
             try
             {
+                var piloto = _mapper.Map<Piloto>(pilotoModelo);
+
                 if (_pilotoRepositorio.Existe(piloto.Id))
                     return StatusCode(409, "Já existe piloto com a mesma identificação");
 
                 _pilotoRepositorio.Adicionar(piloto);
 
-                return CreatedAtRoute("Obter", new { id = piloto.Id }, piloto);
+                var pilotoModeloRetorno = _mapper.Map<PilotoModelo>(piloto);
+                return CreatedAtRoute("Obter", new { id = piloto.Id }, pilotoModeloRetorno);
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "ocorreu um erro interno no sistema. Por favor entre em contato com o suporte");
+                return StatusCode(500, "Ocorreu um erro interno no sistema. Por favor entre em contato com o suporte");
             }
         }
 
         [HttpPut]
-        public IActionResult Atualizar([FromBody] Piloto piloto)
+        public IActionResult Atualizar([FromBody] PilotoModelo pilotoModelo)
         {
             try
             {
-                if (!_pilotoRepositorio.Existe(piloto.Id))
+                if (!_pilotoRepositorio.Existe(pilotoModelo.Id))
                     return NotFound();
+
+                var piloto = _mapper.Map<Piloto>(pilotoModelo);
 
                 _pilotoRepositorio.Atualizar(piloto);
 
@@ -69,12 +80,12 @@ namespace RallyDakar.API.Controllers
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "ocorreu um erro interno no sistema. Por favor entre em contato com o suporte");
+                return StatusCode(500, "Ocorreu um erro interno no sistema. Por favor entre em contato com o suporte");
             }
         }
 
         [HttpPatch]
-        public IActionResult AtualizarParcialmente(int id, [FromBody] JsonPatchDocument<Piloto> patchPiloto)
+        public IActionResult AtualizarParcialmente(int id, [FromBody] JsonPatchDocument<PilotoModelo> patchPilotoModelo)
         {
             try
             {
@@ -83,7 +94,11 @@ namespace RallyDakar.API.Controllers
 
                 var piloto = _pilotoRepositorio.Obter(id);
 
-                patchPiloto.ApplyTo(piloto);
+                var pilotoModelo = _mapper.Map<PilotoModelo>(piloto);
+
+                patchPilotoModelo.ApplyTo(pilotoModelo);
+
+                piloto = _mapper.Map(pilotoModelo, piloto);
 
                 _pilotoRepositorio.Atualizar(piloto);
 
@@ -91,7 +106,7 @@ namespace RallyDakar.API.Controllers
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "ocorreu um erro interno no sistema. Por favor entre em contato com o suporte");
+                return StatusCode(500, "Ocorreu um erro interno no sistema. Por favor entre em contato com o suporte");
             }
         }
 
@@ -112,7 +127,7 @@ namespace RallyDakar.API.Controllers
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "ocorreu um erro interno no sistema. Por favor entre em contato com o suporte");
+                return StatusCode(500, "Ocorreu um erro interno no sistema. Por favor entre em contato com o suporte");
             }
         }
         [HttpOptions]
