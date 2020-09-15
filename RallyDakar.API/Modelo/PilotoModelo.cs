ae4c5cb34e03d5cd84ef64d1a2655eb8e82e813c
RafaelMenezess <rafa.rcm@hotmail.com>
2020-09-15 19:07:15 -0300

    Validações

--- RallyDakar.API/Modelo/PilotoModelo.cs
+++ RallyDakar.API/Modelo/PilotoModelo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,17 @@ namespace RallyDakar.API.Modelo
 {
     public class PilotoModelo
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nome é de preenchimento Obrigatório")]
+        [MinLength(5, ErrorMessage ="Nome deve ter no mínimo 5 caracteres")]
+        [MaxLength(50, ErrorMessage ="Nome não pode ter mais que 50 caracteres")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Sobrenome é de preenchimento Obrigatório")]
+        [MinLength(5, ErrorMessage = "Sobrenome deve ter no mínimo 5 caracteres")]
+        [MaxLength(50, ErrorMessage = "Sobrenome não pode ter mais que 50 caracteres")]
         public string SobreNome { get; set; }
         public int EquipeId { get; set; }
 
